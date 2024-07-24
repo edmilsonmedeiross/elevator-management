95d78af265611592834e5d13a43fc0666c54afae
Eddie <edmilson_jb@hotmail.com>
2024-07-24 02:02:47 -0300

    feat/ -> cria mapeamento pendency ef

---
+++ ElevatorManagementAPI.Api/Data/Mappings/PendencyMapping.cs
@@ -0,0 +1,81 @@
+using ElevatorManagementAPI.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ElevatorManagementAPI.Api.Data.Mappings
+{
+  public class PendencyMapping
+    : IEntityTypeConfiguration<PendencyModel>
+  {
+    public void Configure(
+      EntityTypeBuilder<PendencyModel> builder
+    )
+    {
+      builder.ToTable("Pendencies");
+
+      builder.HasKey(p => p.Id);
+
+      builder
+        .Property(p => p.Status)
+        .IsRequired()
+        .HasConversion<string>()
+        .HasColumnType("varchar(20)");
+
+      builder
+        .Property(p => p.Description)
+        .IsRequired()
+        .HasColumnType("varchar(200)");
+
+      builder
+        .Property(p => p.Picture)
+        .HasColumnType("varchar(100)");
+
+      builder
+        .Property(p => p.ClosedAt)
+        .HasColumnType("datetime");
+
+      builder
+        .Property(p => p.CreatedAt)
+        .HasColumnType("datetime")
+        .HasDefaultValueSql(
+          "CURRENT_TIMESTAMP"
+        );
+
+      builder
+        .Property(p => p.UpdatedAt)
+        .HasColumnType("datetime");
+
+      builder
+        .Property(p => p.UserId)
+        .IsRequired()
+        .HasColumnType("varchar(100)");
+
+      builder
+        .Property(p => p.ElevatorId)
+        .IsRequired()
+        .HasColumnType("varchar(100)");
+
+      builder
+        .Property(p => p.TenantId)
+        .IsRequired()
+        .HasColumnType("varchar(100)");
+
+      builder
+        .HasOne(p => p.User)
+        .WithMany(u => u.Pendencies)
+        .HasForeignKey(p => p.UserId);
+
+      builder
+        .HasOne(p => p.Elevator)
+        .WithMany(e => e.Pendencies)
+        .HasForeignKey(p =>
+          p.ElevatorId
+        );
+
+      builder
+        .HasOne(p => p.Tenant)
+        .WithMany(t => t.Pendencies)
+        .HasForeignKey(p => p.TenantId);
+    }
+  }
+}

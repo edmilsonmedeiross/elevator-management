6937d1700fbdec0832d86589411952898e8f9f94
Eddie <edmilson_jb@hotmail.com>
2024-07-24 02:41:37 -0300

    feat/ -> cria mapeamento para visitsElevators ef

--- ElevatorManagementAPI.Api/Data/Mappings/UserMapping.cs
+++ ElevatorManagementAPI.Api/Data/Mappings/UserMapping.cs
@@ -10,34 +10,6 @@ namespace ElevatorManagementAPI.Api.Data.Mappings
     public void Configure(
       EntityTypeBuilder<UserModel> builder
     )
-    /*
-    public long Id { get; set; }
-    public required string Name { get; set; }
-    public required DocumentType DocumentType { get; set; }
-    public required string DocumentNumber { get; set; }
-    public required string Email { get; set; }
-    public required string Password { get; set; }
-    public required UserRole Role { get; set; }
-    public required string Tel { get; set; }
-    public required bool IsActive { get; set; }
-    public DateTime? PasswordChangedAt { get; set; }
-    public DateTime CreatedAt { get; set; } =
-      DateTime.Now;
-    public DateTime UpdatedAt { get; set; }
-    public required long TenantId { get; set; }
-    public long? InvitedById { get; set; }
-    public UserModel? InvitedBy { get; set; }
-
-    public virtual required TenantModel Tenant { get; set; }
-
-    public virtual ICollection<BuildingModel>? Buildings { get; set; }
-
-    public virtual ICollection<ElevatorModel>? Elevators { get; set; }
-
-    public virtual UserModel? InvitedUser { get; set; }
-
-    public virtual ICollection<PendencyModel>? Pendencies { get; set; }
-    */
     {
       builder.ToTable("Users");
 

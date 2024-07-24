256605ff0e74129967ecfdb4d7b105f15ee2ffa7
Eddie <edmilson_jb@hotmail.com>
2024-07-24 02:07:50 -0300

    fix/ -> corrige formatacao

--- ElevatorManagementAPI.Api/Data/Mappings/AddressMapping.cs
+++ ElevatorManagementAPI.Api/Data/Mappings/AddressMapping.cs
@@ -4,49 +4,61 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ElevatorManagementAPI.Api.Data.Mappings
 {
-  public class AddressMapping : IEntityTypeConfiguration<AddressModel>
+  public class AddressMapping
+    : IEntityTypeConfiguration<AddressModel>
   {
-    public void Configure(EntityTypeBuilder<AddressModel> builder)
+    public void Configure(
+      EntityTypeBuilder<AddressModel> builder
+    )
     {
       builder.ToTable("Address");
       builder.HasKey(a => a.Id);
-      builder.Property(a => a.Street)
-        .IsRequired()
-        .HasColumnType("NVARCHAR")
-        .HasMaxLength(100);
-      builder.Property(a => a.Number)
-        .IsRequired()
-        .HasColumnType("INT");
-      builder.Property(a => a.City)
-        .IsRequired()
-        .HasColumnType("NVARCHAR")
-        .HasMaxLength(100);
-      builder.Property(a => a.State)
-        .IsRequired()
-        .HasColumnType("NVARCHAR")
-        .HasMaxLength(100);
-      builder.Property(a => a.ZipCode)
-        .IsRequired()
-        .HasColumnType("NVARCHAR")
-        .HasMaxLength(100);
-      builder.Property(a => a.District)
-        .IsRequired()
-        .HasColumnType("NVARCHAR")
-        .HasMaxLength(100);
-      builder.Property(a => a.BuildingId)
+      builder
+        .Property(a => a.Street)
         .IsRequired()
         .HasColumnType("varchar(100)");
-      builder.Property(a => a.TenantId)
+      builder
+        .Property(a => a.Number)
+        .IsRequired()
+        .HasColumnType("int");
+      builder
+        .Property(a => a.City)
+        .IsRequired()
+        .HasColumnType("varchar(100)");
+      builder
+        .Property(a => a.State)
+        .IsRequired()
+        .HasColumnType("varchar(100)");
+      builder
+        .Property(a => a.ZipCode)
+        .IsRequired()
+        .HasColumnType("varchar(100)");
+      builder
+        .Property(a => a.District)
+        .IsRequired()
+        .HasColumnType("varchar(100)");
+      builder
+        .Property(a => a.BuildingId)
+        .IsRequired()
+        .HasColumnType("varchar(100)");
+      builder
+        .Property(a => a.TenantId)
         .IsRequired()
         .HasColumnType("varchar(100)");
 
-      builder.HasOne(a => a.Tenant)
+      builder
+        .HasOne(a => a.Tenant)
         .WithOne(t => t.Address)
-        .HasForeignKey<AddressModel>(a => a.TenantId);
+        .HasForeignKey<AddressModel>(
+          a => a.TenantId
+        );
 
-      builder.HasOne(a => a.Building)
+      builder
+        .HasOne(a => a.Building)
         .WithOne(b => b.Address)
-        .HasForeignKey<AddressModel>(a => a.BuildingId);
+        .HasForeignKey<AddressModel>(
+          a => a.BuildingId
+        );
     }
   }
 }
--- ElevatorManagementAPI.Api/Data/Mappings/AssigneeMapping.cs
+++ ElevatorManagementAPI.Api/Data/Mappings/AssigneeMapping.cs
@@ -4,50 +4,67 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ElevatorManagementAPI.Api.Data.Mappings
 {
-  public class AssigneeMapping : IEntityTypeConfiguration<AssigneeModel>
+  public class AssigneeMapping
+    : IEntityTypeConfiguration<AssigneeModel>
   {
-    public void Configure(EntityTypeBuilder<AssigneeModel> builder)
+    public void Configure(
+      EntityTypeBuilder<AssigneeModel> builder
+    )
     {
       builder.ToTable("assignees");
 
       builder.HasKey(a => a.Id);
 
-      builder.Property(a => a.Name)
-          .IsRequired()
-          .HasColumnType("varchar(100)")
-          .HasMaxLength(100);
+      builder
+        .Property(a => a.Name)
+        .IsRequired()
+        .HasColumnType("varchar(100)")
+        .HasMaxLength(100);
 
-      builder.Property(a => a.Tel)
-          .IsRequired()
-          .HasColumnType("varchar(20)")
-          .HasMaxLength(20);
+      builder
+        .Property(a => a.Tel)
+        .IsRequired()
+        .HasColumnType("varchar(20)")
+        .HasMaxLength(20);
 
-      builder.Property(a => a.Email)
-          .IsRequired()
-          .HasColumnType("varchar(100)")
-          .HasMaxLength(100);
+      builder
+        .Property(a => a.Email)
+        .IsRequired()
+        .HasColumnType("varchar(100)")
+        .HasMaxLength(100);
 
-      builder.Property(a => a.CreatedAt)
-          .IsRequired()
-          .HasDefaultValueSql("CURRENT_TIMESTAMP");
+      builder
+        .Property(a => a.CreatedAt)
+        .IsRequired()
+        .HasDefaultValueSql(
+          "CURRENT_TIMESTAMP"
+        );
 
-      builder.Property(a => a.UpdatedAt);
+      builder.Property(a =>
+        a.UpdatedAt
+      );
 
-      builder.Property(a => a.TenantId)
-          .IsRequired()
-          .HasColumnType("varchar(100)");
+      builder
+        .Property(a => a.TenantId)
+        .IsRequired()
+        .HasColumnType("varchar(100)");
 
-      builder.Property(a => a.BuildingId)
-          .IsRequired()
-          .HasColumnType("varchar(100)");
+      builder
+        .Property(a => a.BuildingId)
+        .IsRequired()
+        .HasColumnType("varchar(100)");
 
-      builder.HasOne(a => a.Tenant)
-          .WithMany(t => t.Assignees)
-          .HasForeignKey(a => a.TenantId);
+      builder
+        .HasOne(a => a.Tenant)
+        .WithMany(t => t.Assignees)
+        .HasForeignKey(a => a.TenantId);
 
-      builder.HasMany(a => a.Buildings)
-          .WithOne(b => b.Assignee)
-          .HasForeignKey(b => b.AssigneeId);
+      builder
+        .HasMany(a => a.Buildings)
+        .WithOne(b => b.Assignee)
+        .HasForeignKey(b =>
+          b.AssigneeId
+        );
     }
   }
 }

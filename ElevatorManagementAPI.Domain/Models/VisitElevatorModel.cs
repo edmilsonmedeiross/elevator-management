c69fb2367d22db4574c8e563885d008f6fb3b8f2
Eddie <edmilson_jb@hotmail.com>
2024-07-24 03:06:57 -0300

    fire/ -> deleta arquivos desnecessarios

--- ElevatorManagementAPI.Domain/Models/VisitElevatorModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ElevatorManagementAPI.Domain.Models;
-
-public class VisitElevatorModel
-{
-  public int ElevatorId { get; set; }
-  public int VisitId { get; set; }
-  public ElevatorModel? Elevator { get; set; }
-  public VisitModel? Visit { get; set; }
-}
